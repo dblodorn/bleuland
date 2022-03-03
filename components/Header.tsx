bc4651e98bc02d9f80272ca1e3eb9213da752455
Dain Blodorn Kim <dain@db13.us>
2022-03-02 20:58:17 -0800

    header-fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,35 @@
-<<<<<<< HEAD
 import { css } from '@emotion/react'
 import { useContext } from 'react'
+import { ContractDataContext } from '../context/ContractDataContext'
 import { NavLink } from './utils/NavLink'
 import { ConnectWallet } from './ConnectWallet'
 import { useWalletButton } from '@zoralabs/simple-wallet-provider'
-import { ContractDataContext } from '../context/ContractDataContext'
 
 export const Header = () => {
   const { account } = useWalletButton()
   const { contractData } = useContext(ContractDataContext)
-=======
-import { NavLink } from './utils/NavLink'
-import { ConnectWallet } from './ConnectWallet'
-import { APP_TITLE } from "../utils/env-vars";
-
-export const Header = () => {
->>>>>>> 8da4a1d8aeb7ad0bc7c9cf8e2c87f2934c99d1ae
   return (
     <>
-      <header>
-        <div className='flex-row'>
+      <header className='flex flex-row justify-between'>
+        <div className='flex flex-row'>
           <NavLink passHref href='/'>
-<<<<<<< HEAD
             <a>{contractData?.name}</a>
-=======
-            <a>{APP_TITLE}</a>
->>>>>>> 8da4a1d8aeb7ad0bc7c9cf8e2c87f2934c99d1ae
           </NavLink>
           <NavLink passHref href='/about'>
             <a>About</a>
           </NavLink>
         </div>
-        <div className='flex-row'>
-          <NavLink passHref href='/manage'>
-            <a>Manage</a>
-          </NavLink>
+        <div className='flex flex-row'>
+          {account && (
+            <>
+              <NavLink passHref href='/mint'>
+                <a>Mint</a>
+              </NavLink>
+              <NavLink passHref href='/manage'>
+                <a>Manage</a>
+              </NavLink>
+            </>
+          )}
           <ConnectWallet />
         </div>
       </header>
